b1d8f90ad08499a711b57e98586a537b0d263483
bFraley <bFraley@users.noreply.github.com>
2016-11-17 18:17:52 -0500

    initial commit

---
+++ core/project-source-file-copyright.py
@@ -0,0 +1,11 @@
+class ProjectSourceFileCopyright():
+    def __init__(self):
+        self.project_name = ''
+        self.project_version = ''
+        self.project_description = ''
+        self.project_url = ''
+        self.copyright_text = ''
+        self.license_text = ''
+        self.license_url = ''
+        self.author_names = []
+        
