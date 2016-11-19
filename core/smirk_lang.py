ef6ed78fc4322d723fbaf58783d4aee1c6c24aaa
bFraley <bFraley@users.noreply.github.com>
2016-11-19 14:05:06 -0500

    init files for smirk_lang

---
+++ core/smirk_lang.py
@@ -0,0 +1,14 @@
+"""
+Smirk - Web Application Framework
+Copyright (c) 2016 Brett Fraley
+
+Source Repository
+https://github.com/bFraley/Smirk
+
+MIT License
+https://github.com/bFraley/Smirk/blob/master/LICENSE
+
+File: smirk_lang.py
+
+About: Defines grammar and syntax for Smirk template language.
+"""
---
+++ core/smirk_lang_parse.py
@@ -0,0 +1,14 @@
+"""
+Smirk - Web Application Framework
+Copyright (c) 2016 Brett Fraley
+
+Source Repository
+https://github.com/bFraley/Smirk
+
+MIT License
+https://github.com/bFraley/Smirk/blob/master/LICENSE
+
+File: smirk_lang_parse.py
+
+About: Implements parser for Smirk template language.
+"""
