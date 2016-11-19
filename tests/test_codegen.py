c98716c2606992924e933eb99d70f4e949d8c8a6
bFraley <bFraley@users.noreply.github.com>
2016-11-19 13:55:18 -0500

    child/grandchild generation logic wrks/tested

--- tests/test_codegen.py
+++ tests/test_codegen.py
@@ -65,22 +65,24 @@ def test_HTMLCodeGen():
     # Test HTMLConstruct.add_innertext
     wrapper_div.add_innertext(wrapper_text)
 
-    # HTMLConstruct more elements for child_node tests
-    # 2 Paragraphs (direct children)
-    par1 = HTMLConstruct('p')
-    par2 = HTMLConstruct('p')
+    # Helper 'init_two_paragraphs' defined below.
+    # Tests for child <p> elements with innertext.
+    init_two_paragraphs(wrapper_div)
 
-    # Create inner text values for par1, and par2
-    par1_text = HTMLInnerTextConstruct('Paragraph 1')
-    par2_text = HTMLInnerTextConstruct('Paragraph 2')
+    # Add new div child element with two
+    # <p> grandchildren of wrapper-div
+    new_child_div = HTMLConstruct('div')
 
-    # Add inner text for par1 and par 2
-    par1.add_innertext(par1_text)
-    par2.add_innertext(par2_text)
-    
-    # Test HTMLConstruct.add_child_element
-    wrapper_div.add_child_element(par1)
-    wrapper_div.add_child_element(par2)
+    # Reuse class_attr from above
+    new_child_div.add_attribute(class_attr)
+
+    # Add paragraphs to new child div
+    init_two_paragraphs(new_child_div)
+
+    # Here we should now have a child div,
+    # with two paragraphs that are then 
+    # grandchildren of wrapper-div.
+    wrapper_div.add_child_element(new_child_div)
 
     print(wrapper_div.get_full_element_result())
 
@@ -93,4 +95,36 @@ def test_FileNameConstruct():
 
     new_filename.prepend('previousdir')
     assert(new_filename.name == new_filename.filename_str + new_filename.extension_str)
-    
+
+
+
+
+
+
+
+
+# Test Helper Functions for building HTML Construct elements.
+# -----------------------------------------------------------
+
+# Create two paragraphs and attach to a parent HTML element node.
+def init_two_paragraphs(parent_element):
+    assert(isinstance(parent_element, HTMLConstruct))
+
+    # HTMLConstruct more elements for child_node tests
+    # 2 Paragraphs (direct children)
+    par1 = HTMLConstruct('p')
+    par2 = HTMLConstruct('p')
+
+    # Create inner text values for par1, and par2
+    par1_text = HTMLInnerTextConstruct('Paragraph 1')
+    par2_text = HTMLInnerTextConstruct('Paragraph 2')
+
+    # Add inner text for par1 and par 2
+    par1.add_innertext(par1_text)
+    par2.add_innertext(par2_text)
+
+    # Test HTMLConstruct.add_child_element
+    parent_element.add_child_element(par1)
+    parent_element.add_child_element(par2)
+
+
