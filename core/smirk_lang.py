983cc94e35794e0aeaf603ba110fd0e3ed318142
bFraley <bFraley@users.noreply.github.com>
2017-01-29 10:24:09 -0500

    progress on lang defintion, parsing constructs, and SmirkCoreLib functions

--- core/smirk_lang.py
+++ core/smirk_lang.py
@@ -60,11 +60,11 @@ class SmirkControlToken():
         # for syntax rules of this token type.
         self.__syntax_error_reference_pattern = ''
 
-        def generate_parse_pattern_output(self):
-            pass
+    def generate_parse_pattern_output(self):
+        pass
 
-        def generate_syntax_error_reference(self):
-            pass
+    def generate_syntax_error_reference(self):
+        pass
 
 
 # Defines a comparison boolean token (is, not, and, or)
@@ -98,6 +98,9 @@ class SmirkComparisonToken():
 
 # Defines an extension token that is bound/bindable to a value,
 # is a base implementation type for user defined variables.
+
+# eg: a = 1 would call new SmirkExtensionToken('a', number, 1)
+
 class SmirkExtensionToken():
     def __init__(self, namespace_name, value_type, init_default_value):
         self.namespace_name = namespace_name
@@ -105,13 +108,86 @@ class SmirkExtensionToken():
         self.init_default_value = init_default_value
 
 
+
+
 # Error based token keyword definition classes
 # --------------------------------------------
 
-# Defines developer defined error token
-class SmirkDefineErrorToken():
-    pass
-
 # Defines Smirk implementation defined error token
 class SmirkCoreErrorToken():
-    pass
+    def __init__(self, error_type, error_msg, misc=null):
+        self.error_type = error_type
+        self.error_msg - error_msg
+        self.misc = misc
+
+    def __str__(self):
+        return 'Error Type: {}\nError Message:{} - {}'.format(self.error_type, self.error_msg, self.misc);
+
+
+# Defines developer defined error token
+class SmirkDefineErrorToken(SmirkCoreErrorToken):
+    def __init__(self, user_error_definition):
+        self.user_error_definition = user_error_definition
+
+
+# ------------------------------------------------------------------------------------------------
+# SmirkCoreLib contains functions available to Smirk lang Token types and language constructs above.
+# These are functional helpers and operators that support logic and value manipulation with Smirk code.
+
+class SmirkCoreLib():
+    def __init__(self):
+
+    # Arithmetic
+
+    def add(self, n1, n2):
+        return n1 + n2
+
+    def sub(self, n1, n2):
+        return n1 - n2
+
+    def mult(self, n1, n2):
+        return n1 * n2
+
+    def div(self, n1, n2):
+        return n1 / n2
+
+    def mod(self, n1, n2):
+        return n1 % n2
+
+    # String Manipulation
+
+    def cat(self, s1, s2):
+        return s1 + s2
+
+    def instring(needle, hay):
+        return True if needle in hay else False
+
+    def are_exact_names(s1, s2):
+        return True if s1 == s2 else False
+
+    def are_exact_values(v1, v2):
+        return True if v1 == v2 and type(v1) == type(v2) else False
+
+    # Numeric Formatting and Type Conversions
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
