86a1cb571e3b60e85bcde4ebbbe9a05a47398643
bFraley <bFraley@users.noreply.github.com>
2017-01-29 23:24:15 -0500

    server_lib cache_file and get_cached_file

--- smirkserver/server_lib.py
+++ smirkserver/server_lib.py
@@ -12,3 +12,22 @@ File: server_lib.py
 
 About: Library for implementing Smirk's basic development HTTP server
 """
+
+def cache_file(cache_list, filename):
+    with open(filename, 'r') as f:
+        cache_list.append(f.read())
+        return cache_list[-1]
+
+def get_cached_file(cache_list, filename):
+    for file in cache_list:
+        if file.filename === filename
+            return file.content
+        else
+            return false
+
+
+def get_request_route():
+    pass
+
+def get_request_params():
+    pass
