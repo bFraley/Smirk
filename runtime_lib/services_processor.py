e151c8943f9e265000664e71797a963d2ef6d5ff
bFraley <bFraley@users.noreply.github.com>
2017-02-08 09:24:12 -0500

    view/output/return Processor values

--- runtime_lib/services_processor.py
+++ runtime_lib/services_processor.py
@@ -42,6 +42,14 @@ class Processor():
         self.step_mode = false
         self.log_mode = false
         self.status = 0
+        self.state = [
+            '\nProcessor State Report\n********************',
+            'Event Count: {}\n'.format(self.event_count),
+            'Linear Events: {}\n'.format(len(self.linear_que)),
+            'Selective Events: {}\n'.format(len(self.selective_que)),
+            'Warnings: {}\n'.format(len(self.warnings)),
+            'Errors: {}\n'.format(len(self.errors))
+        ]
 
     # Return and print processor values, called by methods below
     def view_by_list(self, processor_item):
@@ -52,32 +60,34 @@ class Processor():
         return processor_item
 
     def view_state(self):
-        state = [
-            '\nProcessor State Report\n********************',
-            'Event Count: {}\n'.format(self.event_count),
-            'Linear Events: {}\n'.format(len(self.linear_que),
-            'Selective Events: {}\n'.format(len(self.selective_que),
-            'Warnings: {}\n'.format(len(self.warnings),
-            'Errors: {}\n'.format(len(self.errors)]
-
-        for line in state:
+        for line in self.state:
             print(line)
 
-    def view_events(self):
-        pass
+        return self.state
 
+    # Print and return all linear and selective que objects
+    def view_events(self):
+        if (self.log_mode):
+            self.view_by_list(self.linear_que)
+            self.view_by_list(self.selective_que)
+            return list(self.linear_que, self.selective_que)
+
+    # Print and return all linear que objects
     def view_linear_que(self):
         if (self.log_mode):
-            for linear_event in self.linear_que:
-                print(linear_event)
+            self.view_by_list(self.linear_que)
+            return self.linear_que
 
-        return self.linear_que
-
+    # Print and reurn all selective que objects
     def view_selective_que(self):
-        pass
+        if (self.log_mode):
+            self.view_by_list(self.selective_que)
+            return selective_que
 
     def view_warnings(self):
-        pass
+        self.view_by_list(self.warnings)
 
     def view_errors(self):
-        pass
+        self.view_by_list(self.errors)
+
+        
